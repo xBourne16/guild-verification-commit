78ed01b84d74a7ccc09a8853446796af8ce8782b
xBourne16 <oguzzy16@gmail.com>
2026-03-08 18:34:48 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:34:47.499Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T15:34:48.536Z
+Commit: Resolve null pointer exception in user service
