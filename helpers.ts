dbdaa23485a302d95a3a6d5093fc4629428a5f18
xBourne16 <oguzzy16@gmail.com>
2026-03-08 18:34:18 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:34:14.527Z
-Commit: Add multi-language support
+Timestamp: 2026-03-08T15:34:18.607Z
+Commit: Resolve timezone issues in date handling
