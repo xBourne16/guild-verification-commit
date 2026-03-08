dfd32f974195328ed0c542db6d979e8f781422cd
xBourne16 <oguzzy16@gmail.com>
2026-03-08 18:33:56 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:33:53.657Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-08T15:33:56.551Z
+Commit: Fix CSS styling bug on mobile devices
