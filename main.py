6006490b976f309395dde676d2da0add998d62f6
xBourne16 <oguzzy16@gmail.com>
2026-03-08 18:35:05 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:35:04.296Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T15:35:05.277Z
+Commit: Fix memory leak in event handlers
