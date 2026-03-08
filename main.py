452f4d7226ccf884745bf73cea880f90e03c851a
xBourne16 <oguzzy16@gmail.com>
2026-03-08 18:38:03 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:36:49.113Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T15:38:03.777Z
+Commit: Fix broken links in documentation
